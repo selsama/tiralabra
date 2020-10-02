a4c4d6801389e83cfb1555c48a67f5222d205af0
S Salmi <56793802+selsama@users.noreply.github.com>
2020-10-02 18:36:17 +0300

    Luotu ja testattu luokka Jono

---
+++ KarkuKatti/src/main/java/karkukatti/sovelluslogiikka/Jono.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package karkukatti.sovelluslogiikka;
+
+/**
+ * Luokka jonon käsittelemiseen.Luokka tarjoaa metodit jonon loppuun ja alkuun lisäämiseen, ja alusta ja lopusta hakemiseen ja poistamiseen. Kaikki operaatiot toimivat keskimäärin ajassa O(1).
+ * @author salmison
+ * @param <T> jonoon lisättävän olion tyyppi
+ */
+public class Jono<T> {
+    private T[] taulukko;
+    private int eka, vika, muistissa;
+    
+    public Jono() {
+        taulukko = (T[]) new Object[10];
+        eka = 1;
+        vika = 0;
+        muistissa = 0;
+    }
+    
+    public void lisaaLoppuun(T uusi) {
+        vika++;
+        if (vika == taulukko.length) {
+            vika = 0;
+        }        
+        taulukko[vika] = uusi;
+        muistissa++;
+        this.suurennaTaulukkoaJosTarve();
+    }
+    
+    public void lisaaAlkuun(T uusi) {
+        eka--;
+        if (eka < 0) {
+            eka = taulukko.length - 1;
+        }
+        taulukko[eka] = uusi;
+        muistissa++;
+        this.suurennaTaulukkoaJosTarve();
+    }
+    
+    public T haeAlusta() {
+        if (muistissa == 0) {
+            return null;
+        }
+        return taulukko[eka];
+    }
+    
+    public T haeLopusta() {
+        if (muistissa == 0) {
+            return null;
+        }
+        return taulukko[vika];
+    }
+    
+    public T poistaAlusta() {
+        if (muistissa == 0) {
+            return null;
+        }
+        T poistettu = taulukko[eka];
+        eka++;
+        if (eka == taulukko.length) {
+            eka = 0;
+        }
+        muistissa--;
+        this.pienennaTaulukkoaJosTarve();
+        return poistettu;
+    }
+    
+    public T poistaLopusta() {
+        if (muistissa == 0) {
+            return null;
+        }
+        T poistettu = taulukko[vika];
+        vika--;
+        if (vika < 0) {
+            vika+= taulukko.length;
+        }
+        muistissa--;
+        this.pienennaTaulukkoaJosTarve();
+        return poistettu;
+    }
+    
+    public int getKoko() {
+        return muistissa;
+    }
+    
+    private void suurennaTaulukkoaJosTarve() {
+        if (muistissa < taulukko.length) {
+            return;
+        }
+        T[] uusi = (T[]) new Object[taulukko.length * 2];
+        int a = eka;
+        for (int i = 0; i < muistissa; i++) {
+            if (a == taulukko.length) {
+                a = 0;
+            }
+            uusi[i] = taulukko[a];
+            a++;
+        }
+        taulukko = uusi;
+        eka = 0;
+        vika = muistissa - 1;
+        
+    }
+    
+    private void pienennaTaulukkoaJosTarve() {
+        if (muistissa > taulukko.length / 4) {
+            return;
+        }
+        T[] uusi = (T[]) new Object[taulukko.length / 2];
+        int a = eka;
+        for (int i = 0; i < muistissa; i++) {
+            if (a == taulukko.length) {
+                a =- taulukko.length;
+            }
+            uusi[i] = taulukko[a];
+            a++;
+        }
+    }
+    
+}
---
+++ KarkuKatti/src/test/java/sovelluslogiikka/JonoTest.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sovelluslogiikka;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import karkukatti.sovelluslogiikka.Jono;
+import java.util.*;
+/**
+ *
+ * @author salmison
+ */
+public class JonoTest {
+    private Jono<Integer> j;
+    private ArrayDeque<Integer> vert;
+    
+    public JonoTest() {
+        j = new Jono();
+        vert = new ArrayDeque<>();
+    }
+    
+    @Test
+    public void LisaaLoppuunTesti() {
+        int koko = 0;
+        for (int i = 0; i < 30; i++) {
+            j.lisaaLoppuun(i);
+            vert.addLast(i);
+            koko++;
+            assertEquals("Jono palauttaa väärän arvon.", vert.getLast(), j.haeLopusta());
+            assertEquals("jonon koko muuttuu väärin", koko, j.getKoko());
+        }
+    }
+    
+    @Test
+    public void LisaaAlkuunTesti() {
+        int koko = 0;
+        for (int i = 0; i < 30; i++) {
+            j.lisaaAlkuun(i);
+            vert.addFirst(i);
+            koko++;
+            assertEquals("Jono palauttaa väärän arvon.", vert.getFirst(), j.haeAlusta());
+            assertEquals("Jonon koko muuttuu väärin", koko, j.getKoko());
+        }
+    }
+    
+    private void lisaaJonoihin30() {
+        for (int i = 0; i < 30; i++) {
+            j.lisaaAlkuun(i);
+            vert.addFirst(i);
+        }
+    }
+    
+    @Test
+    public void PoistaLopustaTesti() {
+        this.lisaaJonoihin30();
+        int koko = j.getKoko();
+        for (int i = 0; i < 20; i++) {
+            int oikea = vert.pollLast();
+            int a = j.poistaLopusta();
+            koko--;
+            assertEquals("Jono palauttaa väärän arvon " + i, oikea, a);
+            assertEquals("Jonon koko muuttuu väärin", koko, j.getKoko());
+        }
+    }
+
+    @Test
+    public void PoistaAlustaTesti() {
+        this.lisaaJonoihin30();
+        int koko = j.getKoko();
+        for (int i = 0; i < 20; i++) {
+            int oikea = vert.pollFirst();
+            int a = j.poistaAlusta();
+            koko--;
+            assertEquals("Jono palauttaa väärän arvon", oikea, a);
+            assertEquals("Jonon koko muuttuu väärin", koko, j.getKoko());
+        }
+    }
+    
+    @Test
+    public void metodejaSekaisinTesti() {
+        for (int i = 0; i < 50; i++) {
+            j.lisaaLoppuun(i);
+            vert.addLast(i);
+            if (i % 2 == 0) {
+                j.lisaaAlkuun( + 2);
+                vert.addFirst( + 2);
+            }
+            if (i % 7 == 0) {
+               assertEquals("Jonon lopusta poistaminen palauttaa väärän arvon", 
+                       vert.pollLast(), j.poistaLopusta());
+            } else if (i % 5 == 0) {
+                assertEquals("Jonon alusta poistaminen palauttaa väärän arvon", 
+                        vert.pollFirst(), j.poistaAlusta());
+            }
+            if (i % 13 == 3) {
+                assertEquals("Jonon lopusta hakeminen palauttaa väärän arvon",
+                        vert.peekLast(), j.haeLopusta());
+            } else if (i % 13 == 7) {
+                assertEquals("Jonon alusta hakeminen palauttaa väärän arvon", 
+                        vert.peekFirst(), j.haeAlusta());
+            }
+        }
+        while (!vert.isEmpty()) {
+            if (vert.peekFirst() % 2 == 0) {
+                assertEquals("Jonon alusta poistaminen antaa väärän arvon",
+                        vert.pollFirst(), j.poistaAlusta());
+            } else {
+                assertEquals("Jonon lopusta poistaminen antaa väärän arvon",
+                        vert.pollLast(), j.poistaLopusta());
+            }
+        }
+    }
+    
+ }
